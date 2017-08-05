b2b31742c5ff0218dc50980f592d5800c6c9a957
nightrobin <wangrobin1216@hotmail.com>
2017-08-06 03:38:04 +0800

    update css and add input.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,35 @@
 $(function(){
 	//先选中页面中的所有自定义的属性 input[data-rule]
+	var $inputs = $('[data-rule]');
+	var inputs = [];
+	var $form = $("#signup");
+
+	$inputs.each(function(index,node){
+		var tmp = new Input(node) ;
+		inputs.push(tmp);	
+	})
+
+	$form.on('submit',function(e){
+		e.preventDefault();
+		$inputs.trigger('blur');
+		for(var i = 0 ; i < inputs.length ; i++){
+			var item = inputs[i];
+			var r = item.validator.is_valid();
+			if(!r){
+				alert('error')
+				return ;
+			}
+		}
+	})
 	//解析每一个input的验证规则
 	//验证(核心，越核心，越基础的东西我们就要把它写在最前面)
-	var validator = new Validator('a    a',{
-		// maxlength:5,
-		// minlength:3,
-	});
+	// var validator = new Validator('AVZas',{
+	// 	pattern:'^[a-z0-9]*$'
+	// 	// maxlength:5,
+	// 	// minlength:3,
+	// });
 
-	var result = validator.validate_required();
+	// var result = validator.validate_pattern();
 
-	console.log(result);
+	// console.log(result);
 })
--- js/validator.js
+++ js/validator.js
@@ -2,6 +2,27 @@ $(function(){
 	'use strict' ; 
 
 	window.Validator = function(val,rule){
+
+		this.is_valid = function(new_val){
+			var key ; 
+			if(new_val !== undefined)
+			val = new_val;
+
+			if(!rule.required && !val){
+				return true;
+			}
+			for(key in rule){
+				if(key === 'required'){
+					continue;
+				}
+				var r = this['validate_' + key]();
+				if(!r){
+					return false ;
+				}
+				return true;
+			}
+		}
+
 		this.validate_max = function(){
 			pre_max_min();
 			return val <= rule.max;
